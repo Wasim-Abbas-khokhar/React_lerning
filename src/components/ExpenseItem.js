1ffe8fd37b2f92d794878bb396bb905d3ceff4f9
Wasim Abbas <wasim.abbas.khokar@gmail.com>
2021-12-07 12:11:48 +0500

    add state handling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,21 @@
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
+import { useState } from 'react';
 
 function ExpenseItem(props) {
+  let [title, setTitle] = useState(props.title)
+  let clickHandler = () => {
+    setTitle("clicked!!!");
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>click to change title</button>
     </Card>
   );
 }

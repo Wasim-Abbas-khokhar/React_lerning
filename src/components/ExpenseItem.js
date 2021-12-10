17b1c372ab298711c09131ce2b1d7f71b49e9616
Wasim Abbas <wasim.abbas.khokar@gmail.com>
2021-12-10 15:05:06 +0500

    created form and add form data to base array

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -15,7 +15,7 @@ function ExpenseItem(props) {
         <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>click to change title</button>
+      {/* <button onClick={clickHandler}>click to change title</button> */}
     </Card>
   );
 }
